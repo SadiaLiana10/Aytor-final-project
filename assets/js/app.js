b58a2a028f1f41146dfc97bc8fbd41407722b1f9
liana <stunninglia@gmail.com>
2024-06-28 02:30:04 +0600

    latest

--- assets/js/app.js
+++ assets/js/app.js
@@ -93,6 +93,45 @@ $(function(){
       });
     //* ARRIVALS JS END
 
+   
+      
+
+    $('.your-class').slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      arrows:false,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+       
+      ]
+    });
+   
+
 })
+
 
 
